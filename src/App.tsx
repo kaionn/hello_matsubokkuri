db273283c1a94de1257cacb8d6e8f1ce0a535e63
aoinkss <n.kanda@innoqua.jp>
2022-07-31 11:33:24 +0900

    :sparkles: create matubokkuri

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
+import { AppBar, CssBaseline, Toolbar, Typography, Container } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { spacing } from '@mui/system';
+import { Howl } from 'howler';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Matubokkuri from './assets/matubokkuri.png';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#4caf50'
+      }
+    },
+  });
+
+  const hello_sound = new Howl({
+    src: ['./assets/hello.mp3'],
+    onplay: ()=>{
+      console.log("サウンド再生!!");
+    },
+    onstop: ()=>{
+      console.log("サウンド停止!!");
+    },
+    onpause: ()=>{
+      console.log("サウンド一時停止!!");
+    },
+    onend: ()=>{
+      console.log("サウンド終了!!");
+    }
+  });
+
+  const react_matubokkuri = () => {
+    hello_sound.play();
+    alert("\\ｺﾝﾆﾁﾊ/");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              こんにちは
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      <Container fixed sx={{ mt: 6 }} >
+        <img src={Matubokkuri}  alt="松ぼっくり" onClick={react_matubokkuri}/>
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,6 +9,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <button id="button1" />
   </React.StrictMode>
 );
 
